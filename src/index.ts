f825367576ea2a3aa50deb08635c1ef78d1b78d8
Sizimon <siizmonsus@gmail.com>
2025-12-25 03:54:11 +0100

    FEAT: Add graceful shutdown, also added the metric route to the server.

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,9 @@ import app from './server';
 const server = app.listen(config.PORT, () => {
   console.log(`Health Metrics API listening on port ${config.PORT}`)
 })
+
+process.on('SIGTERM', () => {
+  server.close(() => {
+    console.log('Server closed gracefully');
+  });
+});
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import config from './config';
+import metricsRouter from './routes/metricsRoutes/metrics';
 
 const app = express();
 
@@ -9,6 +10,6 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: config.ALLOWED_ORIGINS }))
 
-
+app.use('/api', metricsRouter);
 
 export default app;
